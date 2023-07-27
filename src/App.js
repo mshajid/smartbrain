5a1e42d2b658fa3b440dd7e56125dfc126dfb03a
Shajid <shajid1biz@gmail.com>
2023-07-27 23:11:42 +0530

    Added Few components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>Hello World</h1>
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
       </div>
     )
   }
