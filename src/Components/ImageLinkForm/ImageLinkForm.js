c10144c9cf02c0629baf69ed0ee703d61c117db9
Shajid <shajid1biz@gmail.com>
2023-07-27 23:25:17 +0530

    Added styles to the components

---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+        <p className="f3 center">
+            {'This Magic Brain will detect faces in Your Pictures. Give It a Try!'}
+        </p>
+        <div className='center w-50 br3 shadow5'>
+            <input type='text'className="f4 p2 w-70"/>
+            <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-blue pointer">Detect</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default ImageLinkForm
