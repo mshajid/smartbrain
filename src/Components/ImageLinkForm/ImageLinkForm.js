cc2503e963baa07bd051cb9f7520cbd075d7ddc5
Shajid <shajid1biz@gmail.com>
2023-07-27 23:55:33 +0530

    Added few tachyons styles for center

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -6,9 +6,12 @@ const ImageLinkForm = () => {
         <p className="f3 center">
             {'This Magic Brain will detect faces in Your Pictures. Give It a Try!'}
         </p>
-        <div className='center w-50 br3 shadow5'>
-            <input type='text'className="f4 p2 w-70"/>
-            <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-blue pointer">Detect</button>
+        <div>
+            <div className='form w-50 br3 pa4 center shadow-5'>
+                <input type='text'className="f4 p2 w-70 "/>
+                <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-blue pointer">Detect</button>
+            </div>
+            
         </div>
 
     </div>
