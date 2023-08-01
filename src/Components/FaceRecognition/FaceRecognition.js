09b1dfdb3ef3a123759e660005a75f70f544749d
Shajid <shajid1biz@gmail.com>
2023-08-02 01:55:16 +0530

    Code clean up

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -8,7 +8,6 @@ const FaceRecognition = ({imageUrl, box}) => {
             <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
             <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
         </div>
-        
     </div>
   )
 }
