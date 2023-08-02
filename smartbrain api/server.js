6811be8411d9dc2c3ade05eb376d48a75e5647d7
Shajid <shajid1biz@gmail.com>
2023-08-02 23:06:09 +0530

    Completed few routes and checked with Postman

--- smartbrain api/server.js
+++ smartbrain api/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-
 const app = express()
 app.use(bodyParser.json());
 
@@ -36,7 +35,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send('This is working')
+    res.send(database.users)
 })
 
 app.post('/signin', (req, res) => {
@@ -44,6 +43,48 @@ app.post('/signin', (req, res) => {
         res.json('Success')
     } else {
         res.status(400).json('error login in. Try again!')
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '128',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(`Welcome ${name}, This is your email ${email} that registered with our smartbrain system `)
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach((user) => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user)
+        }
+    })
+    if (!found) {
+        res.status(400).json('There is no such a user.')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body; 
+    let found = false;
+    database.users.forEach((user) => {
+        if(user.id === id) {
+            found=true;
+            user.entries ++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('There is no such a user with the ID')
     }
 })
 
